61e318a7dc0f460e77858f1e8439959b37652a11
Yoruuu00 <144782663+Yoruuu00@users.noreply.github.com>
2025-06-12 19:05:58 +0800

    Delete Laprak5_Web directory

--- Laprak5_Web/HapusBuku.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("Model.php");
-
-if (isset($_GET['id'])) {
-    deleteBuku($_GET['id']);
-}
-
-header("Location: Buku.php");
-exit;
--- Laprak5_Web/HapusPeminjaman.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("Model.php");
-
-if (isset($_GET['id'])) {
-    deletePeminjaman($_GET['id']);
-}
-
-header("Location: Peminjaman.php");
-exit;
